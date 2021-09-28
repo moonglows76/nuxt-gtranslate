47ddab8e4682137263f0a00d25d0e207987a3660
森史憲 <mori@codingdesing.jp>
2021-09-28 09:33:35 +0900

    国旗をクリックするとURLを変更して翻訳するバージョンを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,38 @@
 <template>
   <div class="container">
     <p>東京</p>
+    <p>おはようございます</p>
+    <p>こんにちは</p>
+    <p>おやすみなさい</p>
+    <a href="#googtrans(ja|zh-CN)" @click="changeLang" data-lang="zh-CN" title="Chinese (Simplified)" class="gflag nturl" style="background-position:-300px -0px;"><img src="//gtranslate.net/flags/blank.png" height="32" width="32" alt="Chinese (Simplified)" /></a>
+    <a href="#googtrans(ja|en)" @click="changeLang" data-lang="en" title="English" class="gflag nturl" style="background-position:-0px -0px;"><img src="//gtranslate.net/flags/blank.png" height="32" width="32" alt="English" /></a>
+    <a href="#googtrans(ja|ja)" @click="changeLang" data-lang="ja" title="Japanese" class="gflag nturl" style="background-position:-700px -100px;"><img src="//gtranslate.net/flags/blank.png" height="32" width="32" alt="Japanese" /></a>
+    <a href="#googtrans(ja|ko)" @click="changeLang" data-lang="ko" title="Korean" class="gflag nturl" style="background-position:-0px -200px;"><img src="//gtranslate.net/flags/blank.png" height="32" width="32" alt="Korean" /></a>
 
-    <!-- <a href="#" onclick="doGTranslate('ja|ja');return false;" title="Japanese" class="gflag nturl" style="background-position:-700px -100px;"><img src="https://gtranslate.net/flags/blank.png" height="32" width="32" alt="Japanese" /></a>
-    <a href="#" onclick="doGTranslate('ja|zh-CN');return false;" title="Chinese (Simplified)" class="gflag nturl" style="background-position:-300px -0px;"><img src="https://gtranslate.net/flags/blank.png" height="32" width="32" alt="Chinese (Simplified)" /></a>
-    <a href="#" onclick="doGTranslate('ja|en');return false;" title="English" class="gflag nturl" style="background-position:-0px -0px;"><img src="https://gtranslate.net/flags/blank.png" height="32" width="32" alt="English" /></a>
-    <a href="#" onclick="doGTranslate('ja|ko');return false;" title="Korean" class="gflag nturl" style="background-position:-0px -200px;"><img src="https://gtranslate.net/flags/blank.png" height="32" width="32" alt="Korean" /></a> -->
-
-    <div id="google_translate_element2"></div>
+    <div id="google_translate_element"></div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    changeLang(e){
+      var lang = e.currentTarget.getAttribute('data-lang');
+      window.location = e.currentTarget.getAttribute('href');
+      location.reload();
+    }
+  },
+}
 </script>
 
-<style>
-a.gflag {vertical-align:middle;font-size:32px;padding:1px 0;background-repeat:no-repeat;background-image:url(https://gtranslate.net/flags/32.png);}
+
+<style type="text/css">
+a.gflag {vertical-align:middle;font-size:32px;padding:1px 0;background-repeat:no-repeat;background-image:url(//gtranslate.net/flags/32.png);}
 a.gflag img {border:0;}
-a.gflag:hover {background-image:url(https://gtranslate.net/flags/32a.png);}
+a.gflag:hover {background-image:url(//gtranslate.net/flags/32a.png);}
 #goog-gt-tt {display:none !important;}
 .goog-te-banner-frame {display:none !important;}
 .goog-te-menu-value:hover {text-decoration:none !important;}
 body {top:0 !important;}
-/* #google_translate_element2 {display:none!important;} */
+#google_translate_element {display:none!important;}
 </style>
